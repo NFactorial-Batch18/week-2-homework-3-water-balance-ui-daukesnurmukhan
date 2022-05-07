7959e78e23cfe0dd307ed0587a36d39fc28e874a
daukesnurmukhan <daukesnurmukhan@gmail.com>
2022-05-07 16:11:06 +0600

    Initial Commit

---
+++ WaterBalance/WaterBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterBalanceApp.swift
+//  WaterBalance
+//
+//  Created by Нұрмұхан Дәукес on 07.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WaterBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
